a00e15e6e726c266c809e0dc1390ca010eeb1ebb
Sebastian <Sebastian.ness@gmail.com>
2019-04-26 12:08:37 +0200

    Finished Jobj-Saver-Loader

--- src/main/java/com/hoe/model/datasaving/JobjSaver.java
+++ src/main/java/com/hoe/model/datasaving/JobjSaver.java
@@ -8,23 +8,23 @@ import java.io.*;
  * Serializable reader class that saves the state of a given java object
  */
 public class JobjSaver extends DataSaver implements Serializable {
-    // TODO(1): Add comments
-    // TODO(2): Testing
+
+    // TODO: Make the seri class make a fixed *.ser file.
 
     @Override
     public void saveData(String filename, Database data) throws IOException {
-        readObject(data, filename);
+        saveObject(data, filename);
     }
 
     /**
      * This method saves a object of its current state
      * It's used to save the Database class as this is where the information is stored.
-     * @param data Get's an object of the Database-class to store the information of the current array/arrays
+     * @param data Get's an object of the Database-class to store the information of the current
+     *             made contacts, shows tickets etc.
      */
-    public void readObject(Database data, String filename){
+    public void saveObject(Database data, String filename){
 
         try {
-            // Saves the state of the given object to a "SaveFile.ser"
             FileOutputStream fileOutputStream = new FileOutputStream(filename, false);
             ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
             out.writeObject(data);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module HouseOfEntertainment {
     requires javafx.controls;
     requires javafx.fxml;
-    requires arquillian.container.test.api;
-    requires arquillian.junit.core;
-    requires junit;
 
     opens com.hoe.controller to javafx.fxml;
     exports com.hoe;
